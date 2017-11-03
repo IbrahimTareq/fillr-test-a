1883dcc2389bcfcfbee9a61072930a0c61a309a4
Ibrahim Tareq <ibrahimtareq@live.com>
2017-11-03 18:47:47 +1100

    Task #2 Extract Metadata - Complete

--- extract.js
+++ extract.js
@@ -41,5 +41,11 @@ module.exports.extract = function(window) {
 	        values.push(input_name.toString().trim()); //Whitespace removed
 	    }
 
+	    keys.shift(); //Get rid of the first <tr> with link tags
+	    for(var i = 0; i < keys.length; i++) {
+	        obj[keys[i]] = keys[i] + " " + values[i];
+	    }
+
+	    return obj;
 	}
 }
