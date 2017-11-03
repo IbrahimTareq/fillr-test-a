3b38615983eec532781a15839703a74ff462ec70
Ibrahim Tareq <ibrahimtareq@live.com>
2017-11-03 18:35:08 +1100

    Metadata labels complete

--- extract.js
+++ extract.js
@@ -16,15 +16,27 @@ module.exports.extract = function(window) {
 	    const dom = new JSDOM(html);
 	    const $ = (require('jquery'))(dom.window);
 
+	    //var obj = {};
+    	var keys = [];
+
 	    var table_rows = $("tr");
 	    var inputs = $("input");
 
 	    for(var i = 0; i < table_rows.length; i++){
 	        var label = $(table_rows[i]).children('td').html();
 	        var input_name = String($(inputs[i]).attr('name')); //Convert to string
-        
-	        console.log(label);
-	        console.log(input_name);
+	        var patt = new RegExp("^\\s*(<.*)>$"); //Checks initial trailing space and <> tags
+	        var res = patt.test(label);
+
+	        //If the item is a valid label and is not an HTML tag
+	        if(res == false){
+	            keys.push(label.toString().trim()); //Whitespace removed
+	        }
+
+	        keys.shift(); //Get rid of the first <tr> with link tags
+		    for(var i = 0; i < keys.length; i++) {
+		        console.log(keys[i]);
+		    }
 	    }
 	}
 }
