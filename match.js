6abbc008742af1b32038f8dc286e3ba32c667596
Ibrahim Tareq <ibrahimtareq@live.com>
2017-11-03 18:56:56 +1100

    Task #2 Match Credit Card Controls - Complete

--- match.js
+++ match.js
@@ -1,5 +1,14 @@
 module.exports.match = function(hash) {
+	var patt = new RegExp("Ecom_Payment_Card_(Name|Type|Number|Verification|ExpDate_Day|ExpDate_Month|ExpDate_Year|Protocol)$");
+    var creditCardData = [];
+    var res;
 
-  // Write your solution to Task #2 - Match Metadata task here
-  
+    Object.keys(hash).forEach(function(key) {
+        res = patt.test(hash[key]);
+        if(res == true){
+            creditCardData.push(hash[key]);
+        }
+    });
+
+    return creditCardData;
 }
