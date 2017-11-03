a7a4bb36505e74ca1c50628033f4c2483dc5ded7
Ibrahim Tareq <ibrahimtareq@live.com>
2017-11-03 18:22:30 +1100

    Displaying all table rows and input names successfully

--- extract.js
+++ extract.js
@@ -1,5 +1,30 @@
+const curl = require("curl");
+const jsdom = require("jsdom");
+
 module.exports.extract = function(window) {
   
-  // Write your solution to Task #2 - Extract Metadata task here
+	curl.get(url, null, (err,res,body)=>{
+	    if(res.statusCode == 200){
+	        parseData(body);
+	    } else{
+	        console.log("Error while fetching url");
+	    };
+	});
 
+	function parseData(html){
+	    const {JSDOM} = jsdom;
+	    const dom = new JSDOM(html);
+	    const $ = (require('jquery'))(dom.window);
+
+	    var table_rows = $("tr");
+	    var inputs = $("input");
+
+	    for(var i = 0; i < table_rows.length; i++){
+	        var label = $(table_rows[i]).children('td').html();
+	        var input_name = String($(inputs[i]).attr('name')); //Convert to string
+        
+	        console.log(label);
+	        console.log(input_name);
+	    }
+	}
 }
